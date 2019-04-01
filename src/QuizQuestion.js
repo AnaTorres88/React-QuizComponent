9dce7116caa0bc89472afb7608f4f2d49ba9c621
Ana Laura Torres Gonzalez <ana.torres.gonzalez@accenture.com>
2019-04-01 16:10:36 -0600

    module 5 completed

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 class QuizQuestion extends Component{
+  handleClick(buttonText){
+    if(buttonText === this.props.quiz_question.answer){
+      this.props.showNextQuestionHandler();
+    }
+  }
     render(){
+      const item = this.props.quiz_question.answer_options.map((answer_option, index) =>
+        <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)}/>  );
         return( 
         <main>
             <section>
@@ -9,9 +16,8 @@ class QuizQuestion extends Component{
             </section>
             <section className="buttons">
               <ul>
-                <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]}/>
+                  {item}
                   <li>{this.props.quiz_question.answer_options[0]}</li>
-
               </ul>
             </section>
           </main>
