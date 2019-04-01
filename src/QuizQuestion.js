4e06f5af6a4bf03ca36e6ffa12eb9c8225774941
Ana Laura Torres Gonzalez <ana.torres.gonzalez@accenture.com>
2019-04-01 16:35:10 -0600

    module 7 finished

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton.js';
 class QuizQuestion extends Component{
+    constructor(props){
+      super(props);
+      this.state={incorrectAnswer:false}
+
+  }
   handleClick(buttonText){
     if(buttonText === this.props.quiz_question.answer){
       this.props.showNextQuestionHandler();
+      this.setState({incorrectAnswer:false});
+
+    }else{
+      this.setState({incorrectAnswer:true});
     }
   }
     render(){
@@ -20,6 +29,7 @@ class QuizQuestion extends Component{
                   <li>{this.props.quiz_question.answer_options[0]}</li>
               </ul>
             </section>
+           {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
           </main>
         )
     }
